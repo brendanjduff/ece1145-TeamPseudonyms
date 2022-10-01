d7689094caaa04d3ffc71196b5f76444d22ff0cd
RaheelFarouk <raheelfarouk@gmail.com>
2022-10-01 16:06:31 -0400

    gammaciv action strategy

--- src/main/java/hotciv/variants/BuildCitySettlerActionStrategy.java
+++ src/main/java/hotciv/variants/BuildCitySettlerActionStrategy.java
@@ -2,8 +2,10 @@ package hotciv.variants;
 
 import hotciv.common.SettlerActionStrategy;
 import hotciv.framework.City;
+import hotciv.framework.Player;
 import hotciv.framework.Position;
 import hotciv.framework.Unit;
+import hotciv.standard.CityImpl;
 
 import java.util.Map;
 
@@ -15,6 +17,9 @@ public class BuildCitySettlerActionStrategy implements SettlerActionStrategy {
         Build city - remove settler from world and replace with a city of population size 1.
         Owner of city same as player who owned settler
          */
+
+        city.put(p,new CityImpl(units.get(p).getOwner()));
+        units.remove(p);
     }
     // TODO: Step 5, Implement behavior for GammaCiv
 }
--- src/main/java/hotciv/variants/FortifyArcherActionStrategy.java
+++ src/main/java/hotciv/variants/FortifyArcherActionStrategy.java
@@ -1,7 +1,23 @@
 package hotciv.variants;
 
 import hotciv.common.ArcherActionStrategy;
+import hotciv.framework.Unit;
+import hotciv.standard.UnitImpl;
 
 public class FortifyArcherActionStrategy implements ArcherActionStrategy {
+    @Override
+    public void fortify(Unit unit) {
+        /*
+        Fortify - double defensive strength, cannot move
+        If already fortified, remove fortification
+         */
+        if(!unit.fortified()){
+            unit.setDefensiveStrength(unit.getDefensiveStrength()*2);
+            unit.fortify(true);
+        }else{
+            unit.setDefensiveStrength(unit.getDefensiveStrength()/2);
+        }
+
+    }
     // TODO: Step 5, Implement behavior for GammaCiv
 }
