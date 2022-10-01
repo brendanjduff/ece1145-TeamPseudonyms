b626b6d2f3128a507db2d079633bc0ddf9486bbe
RaheelFarouk <raheelfarouk@gmail.com>
2022-10-01 14:33:21 -0400

    Implements NoSettlerActionStrategy and NoArcherActionStrategy

--- src/main/java/hotciv/variants/NoArcherActionStrategy.java
+++ src/main/java/hotciv/variants/NoArcherActionStrategy.java
@@ -3,5 +3,11 @@ package hotciv.variants;
 import hotciv.common.ArcherActionStrategy;
 
 public class NoArcherActionStrategy implements ArcherActionStrategy {
+    @Override
+    public void fortify(int defensiveStrength) {
+        /*
+        No action taken here
+         */
+    }
     // TODO: Step 2, Implement AlphaCiv behavior here
 }
--- src/main/java/hotciv/variants/NoSettlerActionStrategy.java
+++ src/main/java/hotciv/variants/NoSettlerActionStrategy.java
@@ -1,7 +1,20 @@
 package hotciv.variants;
 
 import hotciv.common.SettlerActionStrategy;
+import hotciv.framework.City;
+import hotciv.framework.Position;
+import hotciv.framework.Unit;
+
+import java.util.Map;
 
 public class NoSettlerActionStrategy implements SettlerActionStrategy {
+
+    @Override
+    public void buildCity(Map<Position, City> city, Map<Position, Unit> units) {
+        /*
+        No action taken for this
+         */
+    }
     // TODO: Step 2, Implement AlphaCiv behavior here
+
 }
