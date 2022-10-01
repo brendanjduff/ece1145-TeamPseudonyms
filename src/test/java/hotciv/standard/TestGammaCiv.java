f799b3f77ee947ae707e8923ca1ca20287bac0e0
RaheelFarouk <raheelfarouk@gmail.com>
2022-10-01 16:06:10 -0400

    gamma civ test cases

--- src/test/java/hotciv/standard/TestGammaCiv.java
+++ src/test/java/hotciv/standard/TestGammaCiv.java
@@ -20,4 +20,19 @@ public class TestGammaCiv {
     }
 
     // TODO: Step 4, Implement tests for GammaCiv behavior for Archer Action Strategy and Settler Action Strategy
+    @Test
+    public void performBuildCitySettlerActionOnR4C3() {
+        assertThat(game, is(notNullValue()));
+        game.performSettlerAction(new Position(4,3));
+
+        assertThat(game.getCityAt(new Position(4,3)).getOwner(), is(Player.RED));  //no change
+    }
+
+    @Test
+    public void performFortifyArcherActionOnR2C0() {
+        assertThat(game, is(notNullValue()));
+        game.performArcherAction(new Position(2,0));
+
+        assertThat(game.getUnitAt(new Position(2,0)).fortified(), is(true));  //no change
+    }
 }
