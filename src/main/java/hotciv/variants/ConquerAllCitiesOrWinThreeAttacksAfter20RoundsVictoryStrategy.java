e202f1e1b7be6c3ba0d5a19a4d6d16592644e639
RaheelFarouk <raheelfarouk@gmail.com>
2022-10-30 17:24:42 -0400

    ZetaCiv Test for conquer all cities

---
+++ src/main/java/hotciv/variants/ConquerAllCitiesOrWinThreeAttacksAfter20RoundsVictoryStrategy.java
@@ -0,0 +1,2 @@
+package hotciv.variants;public class ConquerAllCitiesOrWinThreeAttacksAfter20RoundsVictoryStrategy {
+}
--- src/test/java/hotciv/standard/TestZetaCiv.java
+++ src/test/java/hotciv/standard/TestZetaCiv.java
@@ -1,8 +1,17 @@
 package hotciv.standard;
 
 import hotciv.factory.ZetaCivFactory;
+import hotciv.framework.MutableCity;
 import hotciv.framework.MutableGame;
+import hotciv.framework.Player;
+import hotciv.framework.Position;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
 
 public class TestZetaCiv {
 
@@ -12,4 +21,23 @@ public class TestZetaCiv {
   public void setUp() {
     game = new GameImpl(new ZetaCivFactory());
   }
+
+  @Test
+  public void conquerAllCitiesToWin() {
+    assertThat(game, is(notNullValue()));
+    assertThat(game.getWinner(), is(nullValue()));
+    ((MutableCity) game.getCityAt(new Position(4, 1))).setOwner(Player.RED);
+    assertThat(game.getWinner(), is(Player.RED));
+  }
+
+  @Test
+  public void win3AttacksToWin() {
+    assertThat(game, is(notNullValue()));
+    for (int i = 0; i < 20; i++) {
+      game.endOfTurn();
+    }
+    //TODO: add test case to test out 3 wins
+
+  }
+
 }
