d6c917452461a1df8af64149aa312cb3bf9e7009
brendanjduff <brendanjduff@gmail.com>
2022-10-31 16:32:30 -0400

    Refactor unit action strategies in preparation for UFO action

---
+++ src/main/java/hotciv/variants/BuildCityUnitActionStrategy.java
@@ -0,0 +1,15 @@
+package hotciv.variants;
+
+import hotciv.common.UnitActionStrategy;
+import hotciv.framework.MutableGame;
+import hotciv.framework.Position;
+import hotciv.standard.CityImpl;
+
+public class BuildCityUnitActionStrategy implements UnitActionStrategy {
+
+  @Override
+  public void performAction(Position p, MutableGame game) {
+    game.getCities().put(p, new CityImpl(game.getUnits().get(p).getOwner()));
+    game.getUnits().remove(p);
+  }
+}
--- src/main/java/hotciv/variants/FortifyAndBuildCityActionStrategy.java
+++ src/main/java/hotciv/variants/FortifyAndBuildCityActionStrategy.java
@@ -5,24 +5,19 @@ import hotciv.framework.GameConstants;
 import hotciv.framework.MutableGame;
 import hotciv.framework.MutableUnit;
 import hotciv.framework.Position;
-import hotciv.standard.CityImpl;
 
 public class FortifyAndBuildCityActionStrategy implements UnitActionStrategy {
+
+  UnitActionStrategy settler = new BuildCityUnitActionStrategy();
+  UnitActionStrategy archer = new FortifyUnitActionStrategy();
 
   @Override
   public void performAction(Position p, MutableGame game) {
     MutableUnit unit = game.getUnits().get(p);
     if (unit.getTypeString().equals(GameConstants.ARCHER)) {
-      if (unit.isMovable()) {
-        unit.setMovable(false);
-        unit.setDefensiveStrength(unit.getDefensiveStrength() * 2);
-      } else {
-        unit.setMovable(true);
-        unit.setDefensiveStrength(unit.getDefensiveStrength() / 2);
-      }
+      archer.performAction(p, game);
     } else if (unit.getTypeString().equals(GameConstants.SETTLER)) {
-      game.getCities().put(p, new CityImpl(unit.getOwner()));
-      game.getUnits().remove(p);
+      settler.performAction(p, game);
     }
   }
 }
