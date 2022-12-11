47c283f150f41736e558fea645660d221e5e04c2
RaheelFarouk <raheelfarouk@gmail.com>
2022-12-11 16:49:23 -0500

    Fix EndOfTurnTool

--- src/main/java/hotciv/tools/EndOfTurnTool.java
+++ src/main/java/hotciv/tools/EndOfTurnTool.java
@@ -4,6 +4,7 @@ import hotciv.factory.GameFactory;
 import hotciv.framework.Game;
 import hotciv.framework.MutableGame;
 import hotciv.standard.GameImpl;
+import hotciv.view.GfxConstants;
 import minidraw.standard.NullTool;
 
 import java.awt.event.MouseEvent;
@@ -25,8 +26,12 @@ public class EndOfTurnTool extends NullTool {
     public void mouseUp(MouseEvent e, int x, int y) {
         //TODO: Check for click on top shield on age section
         //30,7 is the position of the shield
-        //System.out.println("X:"+ x+ "   Y:"+ y);
-        if(x==30 && y==7){
+//        System.out.println("X:"+ GfxConstants.getYFromRow(x) + "   Y:"+ GfxConstants.getXFromColumn(y));
+//        System.out.println("X:"+ x + "   Y:"+ y);
+//        System.out.println();
+        if((x>GfxConstants.TURN_SHIELD_X && x<GfxConstants.TURN_SHIELD_X+GfxConstants.TILESIZE)
+                && (y>GfxConstants.TURN_SHIELD_Y && y<GfxConstants.TURN_SHIELD_Y+GfxConstants.TILESIZE) ){
+//            System.out.println("Passed");
             game.endOfTurn();
         }
     }
